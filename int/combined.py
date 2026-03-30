d8d58249e59c220d4fa61eededede641285e65c1
Amal Jose <amal446446@gmail.com>
2026-03-30 12:47:35 +0530

    Solved question 11

--- int/combined.py
+++ int/combined.py
@@ -2,12 +2,20 @@ from collections import Counter, defaultdict
 import heapq
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from leetc.helpers import ListNode, buildNodes, leetcode_output, print_array_with_pointers, print_matrix, showNodes
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from leetc.helpers import (
+	ListNode,
+	buildNodes,
+	leetcode_output,
+	print_array_with_pointers,
+	print_matrix,
+	showNodes,
+)
+
 
 class NeetCodeBlindQs(object):
-	
 
 	def hasDuplicate(self, nums):
 		# 217 easy
@@ -20,10 +28,10 @@ class NeetCodeBlindQs(object):
 			else:
 				myMap[i] = True
 		return False
-	
+
 	def isAnagram(self, s, t):
 		# 242 easy
-		print(s,t)
+		print(s, t)
 		if len(s) != len(t):
 			return False
 		myMapS = {}
@@ -31,17 +39,17 @@ class NeetCodeBlindQs(object):
 		for i in range(len(s)):
 			myMapS[s[i]] = 1 + myMapS.get(s[i], 0)
 			myMapT[t[i]] = 1 + myMapT.get(t[i], 0)
-		
+
 		return myMapS == myMapT
-			
-	def twoSum(self,nums,target):
+
+	def twoSum(self, nums, target):
 		# 1 easy
 		print(nums)
 		print(target)
 		myMap = {}
 		for i in range(len(nums)):
 			curr = nums[i]
-			diff =  target - curr 
+			diff = target - curr
 			if diff in myMap:
 				return [myMap[diff], i]
 			myMap[curr] = i
@@ -61,60 +69,62 @@ class NeetCodeBlindQs(object):
 		# 347 medium
 		myMap = {}
 		freq = [[] for i in range(len(nums) + 1)]
-		
+
 		for n in nums:
 			myMap[n] = myMap.get(n, 0) + 1
-		for n,c in myMap.items():
+		for n, c in myMap.items():
 			freq[c].append(n)
 		print(freq)
 		output = []
 		counter = 0
-		for i in range(len(freq)-1, 0 ,-1):
+		for i in range(len(freq) - 1, 0, -1):
 			for j in freq[i]:
 				output.append(j)
-				counter +=1
+				counter += 1
 				if counter >= k:
 					return output
 		print(output, counter)
-	
+
 	def encode_decode(self, strs):
 		# 271 medium
 		def encode(self, strs):
 			res = ""
 			for s in strs:
-				res+=str(len(s))+"#"+s
+				res += str(len(s)) + "#" + s
 			return res
+
 		def decode(self, s):
 			print(s)
 			res, i = [], 0
 			while i < len(s):
 				j = i
 				while s[j] != "#":
-					j+=1
+					j += 1
 				length = int(s[i:j])
-				word = s[j+1: j + length + 1]
+				word = s[j + 1 : j + length + 1]
 				res.append(word)
 				i = j + 1 + length
 			return res
+
 		return decode(self, encode(self, strs))
-	
+
 	def productExceptSelf(self, nums):
 		# 238 medium
 		print(nums)
 		leng = len(nums)
 		res = [1] * leng
 		# better solution
-		pref =1
+		pref = 1
 		for i in range(leng):
 			res[i] = pref
 			pref *= nums[i]
 			print(res, pref)
 		postf = 1
 		print("-----------")
-		for i in range(leng -1 , -1, -1):
-			res[i]*= postf
+		for i in range(leng - 1, -1, -1):
+			res[i] *= postf
 			postf *= nums[i]
-			
+
 		return res
 
 	def longestConsecutive(self, nums):
@@ -134,7 +144,7 @@ class NeetCodeBlindQs(object):
 	def isPalindrome(self, s):
 		# 125 easy
 		print(s)
-		left,right = 0, len(s)-1
+		left, right = 0, len(s) - 1
 		while left < right:
 			if not s[left].isalnum():
 				left += 1
@@ -147,19 +157,19 @@ class NeetCodeBlindQs(object):
 			left += 1
 			right -= 1
 		return True
-	
+
 	def threeSum(self, nums):
 		# 15 medium
-		res =[]
+		res = []
 		nums.sort()
 		print(nums)
 
 		for i in range(len(nums)):
-			if i > 0 and nums[i-1] == nums[i]:
+			if i > 0 and nums[i - 1] == nums[i]:
 				continue
 			print(nums[i])
-			j=i+1
-			k = len(nums) -1
+			j = i + 1
+			k = len(nums) - 1
 
 			while j < k:
 				sum = nums[i] + nums[j] + nums[k]
@@ -170,15 +180,25 @@ class NeetCodeBlindQs(object):
 				else:
 					res.append([nums[i], nums[j], nums[k]])
 					j += 1
-					while nums[j] == nums[j -1] and j < k:
+					while nums[j] == nums[j - 1] and j < k:
 						j += 1
-				
 
 		return res
 
 	def maxArea(self, heights):
 		# 11 medium
 		print(heights)
+		r = len(heights) - 1
+		l = 0
+		maxx = 0
+		while l < r:
+			print(heights[l], heights[r])
+			maxx = max(maxx, (r - l) * min(heights[l], heights[r]))
+			if(heights[l] < heights[r]):
+				l+=1
+			else:
+				r -= 1
+		return maxx
 
 
 
@@ -187,17 +207,11 @@ class NeetCodeBlindQs(object):
 
 s = NeetCodeBlindQs()
 
-test_arg1 = [1,7,2,5,4,7,3,6]
+test_arg1 = [1, 7, 2, 5, 4, 7, 3, 6]
 test_arg2 = 1
 passes = test_arg1
-out = leetcode_output( 11, s.maxArea, passes) #  // Output: 36
+out = leetcode_output(11, s.maxArea, passes)  #  // Output: 36
 # print(out)
-
-
-
-
-
-
 
 
 # leetcode_output( 217,s.hasDuplicate, [1,2,3,1]) #  // Output: True
@@ -210,3 +224,4 @@ out = leetcode_output( 11, s.maxArea, passes) #  // Output: 36
 # leetcode_output( 128, s.longestConsecutive, [100,4,200,1,3,2]) #  // Output: 4
 # leetcode_output( 125, s.isPalindrome, "Was it a car or a cat I saw?") #  // Output: True
 # leetcode_output( 15, s.threeSum,  [-1,0,1,2,-1,-4]) #  // Output: [[-1,-1,2],[-1,0,1]]
+# leetcode_output(11, s.maxArea, [1, 7, 2, 5, 4, 7, 3, 6])  #  // Output: 36
